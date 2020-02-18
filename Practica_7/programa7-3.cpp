237a4a87efd6cfe511580a53a3184cc1f3ade35a
Garavirod <rodrigogarciaavila26@gmail.com>
2020-02-17 20:47:23 -0600

    p7-2

---
+++ Practica_7/programa7-3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include <algorithm>
+using namespace std;
+#define N 100000
+
+string generteWord(){
+    string t = "";
+    char w1 = (char)(65+rand()%(91-65));
+    char w2 = (char)(65+rand()%(91-65));
+    char w3 = (char)(65+rand()%(91-65));
+    t += w1;
+    t += w2;
+    t += w3;
+    return t;
+}
+
+
+int main(int argc, char const *argv[])
+{
+     // EJERCICO 3
+    srand (time(NULL));
+    string cadenota = "";
+
+    int counter = 0;
+    for(int i =0; i<N; i++){
+        string q = generteWord();
+        // cadenota += q;
+        // cadenota += " ";
+        if(q.compare("IPN")==0)
+            counter++;
+    }
+
+    cout<<counter<<endl;
+    return 0;
+}
